02b9c43d40e7fe20a006489ae140789eacc00398
Charl P. Botha <cpbotha@vxlabs.com>
2023-07-14 17:38:47 +0200

    Add virtualenv

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo pip3 install --upgrade torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-sudo pip3 install --upgrade huggingface-hub
-sudo pip3 install --upgrade transformers einops accelerate
-sudo pip3 install xformers
+mkdir -p ~/.virtualenvs
+python3 -m venv ~/.virtualenvs/transformers
+source ~/.virtualenvs/transformers/bin/activate
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install --upgrade huggingface-hub transformers einops accelerate xformers
